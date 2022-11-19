636d6f51b0b832fa02939165cce8c47c97ce521e
ana-adriana <103989277+ana-adriana@users.noreply.github.com>
2022-11-19 10:49:21 +0000

    Amended personal portfolio URL

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
     Weather App
      <footer>This project is coded by 
       <a href="https://gleeful-yeot-054adb.netlify.app/index.html"target="_blank"rel="noreferrer">
-        AAL
+        AAL{""}
         </a> 
-        {""}
+        
         <a href="https://github.com/ana-adriana/react-weather-app" target="_blank" rel="noreferrer">
        and is open-sourced on GitHub
       </a>
