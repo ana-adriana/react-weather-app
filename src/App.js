fe275456a0b9370490497bf081b1231373e4a612
ana-adriana <103989277+ana-adriana@users.noreply.github.com>
2022-11-19 10:48:03 +0000

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
   return (
     <div className="App">
     Weather App
-      
+     <footer>This project is coded by 
+      <a href="https://gleeful-yeot-054adb.netlify.app/index.html"target="_blank"rel="noreferrer">
+        AAL
+        </a> 
+        {""}
+        <a href="https://github.com/ana-adriana/react-weather-app" target="_blank" rel="noreferrer">
+       and is open-sourced on GitHub
+      </a>
+      </footer> 
     </div>
   );
 }
